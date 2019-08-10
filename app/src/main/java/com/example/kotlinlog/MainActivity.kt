aeb3429c5e280bf7a5cda7ef70bd8555ac233476
shou.kadekaru <kadekaru12345@gmail.com>
2019-08-10 21:28:35 +0900

    最初のコミット

---
+++ app/src/main/java/com/example/kotlinlog/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.kotlinlog
+
+import android.support.
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var num =10
+        Log.d("kotlintest",Integer.toString(num))
+        num=50
+        Log.d("kotlintest",num.toString())
+    }
+}
