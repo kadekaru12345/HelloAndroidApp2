20d9383748a7a70d70b3decc8244ed0980b355a7
shou.kadekaru <kadekaru12345@gmail.com>
2019-08-11 23:19:32 +0900

    比較演算子・条件分岐

--- app/src/main/java/com/example/kotlinlog/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlog/MainActivity.kt
@@ -11,9 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var num =10
-        Log.d("kotlintest",Integer.toString(num))
-        num=50
-        Log.d("kotlintest",num.toString())
+        val num=60
+
+        if (num>=90) {
+            Log.d("kotlintest","優")
+        }else if (num>=75){
+            Log.d("kotlintest","良")
+        }else if (num>=60){
+            Log.d("kotlintest","可")
+        }else
+           Log.d("kotlintest","不可")
+
     }
 }
